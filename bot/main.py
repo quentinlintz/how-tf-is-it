66490d9d5b1c48aac26c1ae10672d71690171948
Quentin Lintz <quentinlintz@outlook.com>
2024-10-26 22:09:40 -0400

    Remove DEBUG flag

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,4 @@
 import os
-import random
-import time
 
 import tweepy
 from dotenv import load_dotenv
@@ -14,7 +12,6 @@ CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_SECRET = os.getenv("ACCESS_SECRET")
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
-DEBUG = os.getenv("DEBUG")
 
 client = tweepy.Client(
     bearer_token=BEARER_TOKEN,
@@ -25,10 +22,7 @@ client = tweepy.Client(
 )
 
 if __name__ == "__main__":
-    delay = random.randint(0, 1800)  # Random delay (0-30 minutes)
     timezone = get_timezone()
     new_post = create_post(timezone)
-    print(f"Posting: {new_post}\nDelay: {delay}s")
-    if not DEBUG:
-        time.sleep(delay)
-        client.create_tweet(text=new_post)
+    print(new_post)
+    client.create_tweet(text=new_post)
